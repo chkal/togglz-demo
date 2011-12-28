899e5b996c5fea9c457a91530c96f816485ac7e9
Christian Kaltepoth <christian@kaltepoth.de>
2011-12-28 15:04:35 +0100

    Fixed demo

--- src/main/java/de/chkal/feature4j/demo/DemoConfiguration.java
+++ src/main/java/de/chkal/feature4j/demo/DemoConfiguration.java
@@ -2,11 +2,14 @@ package de.chkal.feature4j.demo;
 
 import java.io.File;
 
+import javax.enterprise.context.ApplicationScoped;
+
 import de.chkal.togglz.core.Feature;
 import de.chkal.togglz.core.config.FeatureManagerConfiguration;
 import de.chkal.togglz.core.repository.FeatureStateRepository;
 import de.chkal.togglz.core.repository.file.FileBasedRepository;
 
+@ApplicationScoped
 public class DemoConfiguration implements FeatureManagerConfiguration {
 
     @Override
--- src/main/java/de/chkal/feature4j/demo/DemoFeatures.java
+++ src/main/java/de/chkal/feature4j/demo/DemoFeatures.java
@@ -2,13 +2,16 @@ package de.chkal.feature4j.demo;
 
 import de.chkal.togglz.core.Feature;
 import de.chkal.togglz.core.annotation.EnabledByDefault;
+import de.chkal.togglz.core.annotation.Label;
 import de.chkal.togglz.core.holder.FeatureManagerHolder;
 
 public enum DemoFeatures implements Feature {
 
     @EnabledByDefault
+    @Label("First Feature")
     FEATURE_ONE,
     
+    @Label("Second Feature")
     FEATURE_TWO;
     
     public boolean isEnabled() {
