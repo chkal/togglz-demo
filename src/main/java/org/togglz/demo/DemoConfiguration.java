a452fc1a13ee778556af33aadb19787f8f35ad1f
Christian Kaltepoth <christian@kaltepoth.de>
2013-02-27 15:45:06 +0100

    Always allow console access

--- src/main/java/org/togglz/demo/DemoConfiguration.java
+++ src/main/java/org/togglz/demo/DemoConfiguration.java
@@ -9,8 +9,9 @@ import org.togglz.core.Feature;
 import org.togglz.core.manager.TogglzConfig;
 import org.togglz.core.repository.StateRepository;
 import org.togglz.core.repository.file.FileBasedStateRepository;
+import org.togglz.core.user.FeatureUser;
+import org.togglz.core.user.SimpleFeatureUser;
 import org.togglz.core.user.UserProvider;
-import org.togglz.servlet.user.ServletUserProvider;
 
 @ApplicationScoped
 @Component
@@ -25,7 +26,12 @@ public class DemoConfiguration implements TogglzConfig {
     }
 
     public UserProvider getUserProvider() {
-        return new ServletUserProvider("admin");
+        return new UserProvider() {
+            @Override
+            public FeatureUser getCurrentUser() {
+                return new SimpleFeatureUser("admin", true);
+            }
+        };
     }
 
 }
