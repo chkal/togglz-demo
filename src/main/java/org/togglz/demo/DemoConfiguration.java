4703d748f99e0ccbd1fc652ffe7aff542733eb0d
Christian Kaltepoth <christian@kaltepoth.de>
2013-02-27 17:26:50 +0100

    Better use the InMemoryStateRepository for the demo

--- src/main/java/org/togglz/demo/DemoConfiguration.java
+++ src/main/java/org/togglz/demo/DemoConfiguration.java
@@ -1,12 +1,10 @@
 package org.togglz.demo;
 
-import java.io.File;
-
 import org.springframework.stereotype.Component;
 import org.togglz.core.Feature;
 import org.togglz.core.manager.TogglzConfig;
 import org.togglz.core.repository.StateRepository;
-import org.togglz.core.repository.file.FileBasedStateRepository;
+import org.togglz.core.repository.mem.InMemoryStateRepository;
 import org.togglz.core.user.FeatureUser;
 import org.togglz.core.user.SimpleFeatureUser;
 import org.togglz.core.user.UserProvider;
@@ -19,7 +17,7 @@ public class DemoConfiguration implements TogglzConfig {
     }
 
     public StateRepository getStateRepository() {
-        return new FileBasedStateRepository(new File("features.properties"));
+        return new InMemoryStateRepository();
     }
 
     public UserProvider getUserProvider() {
